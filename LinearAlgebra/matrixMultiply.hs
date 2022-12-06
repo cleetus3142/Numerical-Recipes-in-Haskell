7ac3a8ddba17d87876d8893082424d636e38413e
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-06 15:43:49 -0700

    Update matrixMultiply.hs
    Added additional comments on dot product and scalar multiplication

--- LinearAlgebra/matrixMultiply.hs
+++ LinearAlgebra/matrixMultiply.hs
@@ -10,6 +10,16 @@ ghci> b = [[1],[1],[1]]
 ghci> mmult a b
 [[6],[15],[24]]
 ghci> 
+To form the dot product do:
+ghci> a = [[1,2,3]]
+ghci> b = [[1],[1],[1]]
+ghci> mmult a b
+[[6]]
+To multiply a row vector by a scalar (2 in this case) do:
+ghci> g = [[2]]
+ghci> mmult g a
+[[2,4,6]]
+ghci>
 -}
 
 import Data.List
