6b95b8da6b3e38449985887c1478b0d7eb470ca5
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-05 18:37:55 -0700

    Update trapezoidIntg.hs
    Added attribution (Scott Walck) and added ghci example.

--- Integration/trapezoidIntg.hs
+++ Integration/trapezoidIntg.hs
@@ -1,4 +1,16 @@
 -- Computes the integral by the trapezoid rule
+-- This is from Scott Walck's "Learn Physics with Functional Programming"
+{- To run this program do:
+$ ghci  
+Loaded package environment from /home/cleetus/.ghc/x86_64-openbsd-9.2.4/environments/default
+GHCi, version 9.2.4: https://www.haskell.org/ghc/  :? for help
+ghci> :load trapezoidIntg.hs 
+[1 of 1] Compiling Main             ( trapezoidIntg.hs, interpreted )
+Ok, one module loaded.
+ghci> integral 0.05 sqrd 0.0 3.0
+9.001251
+ghci> 
+-}
 
 sqrd :: Float -> Float
 sqrd x = x^2
