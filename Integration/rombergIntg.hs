4d7d5d7f3cf84db53830277f57ec0ee437f814de
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-08 05:54:15 -0700

    Update rombergIntg.hs
    Correction to comment

--- Integration/rombergIntg.hs
+++ Integration/rombergIntg.hs
@@ -1,7 +1,7 @@
 -- Computes the integral by Romberg Integration scheme
--- The integral is approximately I ~ I2n + 1/3*(I2n - In) = 4/3*I2n - 1/3*IN
+-- The integral is approximately I ~ I2n + 1/3*(I2n - In) = 4/3*I2n - 1/3*In
 -- I is the desired integral, In is the integral for step size dt
--- I2n is the integral for dt/2.0
+-- I2n is the integral for step size dt/2.0
 -- romb uses the Trapezoid rule program trapezoidIntg.hs 
 -- BSD 2-Clause License Copyright -- (c) 2022, Jonathan Drews
 
