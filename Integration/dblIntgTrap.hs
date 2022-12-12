24bd1197b55fc8d5e6afe4162ba8cf39f8a3090e
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-12 15:05:45 -0700

    Update dblIntgTrap.hs
    Changed from dtx, dty to dx and dy

--- Integration/dblIntgTrap.hs
+++ Integration/dblIntgTrap.hs
@@ -32,20 +32,21 @@ The derivation of the routine:
   |a|_|_|_|_|a|
 
 The corners (a) are given by:
-dt^2*(f x1 y1  + f x2 y1  + f x1 y2 + f x2 y2)/4.0
+dx*dy*(f x1 y1  + f x2 y1  + f x1 y2 + f x2 y2)/4.0
 The bottom row between the (a) is give by
-(sum [ (f x y1)*dt^2 | x <- [x1+dt,x1+2*dt..x2-dt]])/2.0
+(sum [ (f x y1)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx]])/2.0
 The top row between the (a) is given by
-(sum [ (f x y2)*dt^2 | x <- [x1+dt,x1+2*dt..x2-dt]])/2.0
+(sum [ (f x y2)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx]])/2.0
 The left column between the (a) is given by
-(sum [ (f x1 y)*dt^2 | y <- [y1+dt, y1+2*dt..y2-dt]])/2.0
+(sum [ (f x1 y)*dx*dy | y <- [y1+dy, y1+2*dy..y2-dy]])/2.0
 The right column between the (a) is given by
-(sum [ (f x2 y)*dt^2 | y <- [y1+dt, y1+2*dt..y2-dt]])/2.0
+(sum [ (f x2 y)*dx*dy | y <- [y1+dy, y1+2*dy..y2-dy]])/2.0
 Finally, the interior area is given by:
-sum [ (f x y)*dt^2 | x <- [x1+dt,x1+2*dt..x2-dt], y <- [y1+dt, y1+2*dt..y2-dt]]
+sum [ (f x y)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]]
 Adding these all up you get your integral.
 -}
 -- BSD 2-Clause License Copyright -- (c) 2022, Jonathan Drews
+
 type R=Float
 
 
@@ -55,12 +56,12 @@ dblIntg ::  Int -> (R -> R -> R) -> R -> R -> R -> R -> R
 -- are the limits of integration.
 
 dblIntg n f x1 x2 y1 y2  =
-  let dtx=(x2-x1)/fromIntegral(2*n)
-      dty=(y2-y1)/fromIntegral(2*n)
-  in dtx*dty*(f x1 y1  + f x2 y1  + f x1 y2 + f x2 y2)/4.0 +
-  (sum [ (f x y1)*dtx*dty | x <- [x1+dtx,x1+2*dtx..x2-dtx]])/2.0 +
-  (sum [ (f x y2)*dtx*dty | x <- [x1+dtx,x1+2*dtx..x2-dtx]])/2.0 +
-  (sum [ (f x1 y)*dtx*dty | y <- [y1+dty, y1+2*dty..y2-dty]])/2.0 +
-  (sum [ (f x2 y)*dtx*dty | y <- [y1+dty, y1+2*dty..y2-dty]])/2.0 +
-  sum [ (f x y)*dtx*dty | x <- [x1+dtx,x1+2*dtx..x2-dtx], y <- [y1+dty, y1+2*dty..y2-dty]]
+  let dx=(x2-x1)/fromIntegral(2*n)
+      dy=(y2-y1)/fromIntegral(2*n)
+  in dx*dy*(f x1 y1  + f x2 y1  + f x1 y2 + f x2 y2)/4.0 +
+  (sum [ (f x y1)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx]])/2.0 +
+  (sum [ (f x y2)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx]])/2.0 +
+  (sum [ (f x1 y)*dx*dy | y <- [y1+dy, y1+2*dy..y2-dy]])/2.0 +
+  (sum [ (f x2 y)*dx*dy | y <- [y1+dy, y1+2*dy..y2-dy]])/2.0 +
+  sum [ (f x y)*dx*dy | x <- [x1+dx,x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]]
   
