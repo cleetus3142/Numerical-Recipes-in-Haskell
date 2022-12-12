9c22bbf7a2428d4a5f47e9d7bbbab19e4246bff6
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-12 13:52:55 -0700

    Update tripleIntgTrap.hs
    Spacing set to correspond to exact number of intervals between limits of integration. The old way of specifying the increment (i.e. dt = 0.01) caused the tists, in the list comprehensions, to come up short of the upper limit of integration.

--- Integration/tripleIntgTrap.hs
+++ Integration/tripleIntgTrap.hs
@@ -16,38 +16,41 @@ or in cylindrical coordinates
 
 
 
-NOTE: This routine only works for constant limits of integration.
+NOTE: This routine only works for constant limits of integration. 
 
 -}
 -- BSD 2-Clause License Copyright -- (c) 2022, Jonathan Drews
 
--- Try to use Float instead of Double as Double	precision really
+-- Try to use Float instead of Double as Double precision really
 -- makes for long run times.
 
 type R=Double
 
-trplIntg ::  R -> (R -> R -> R -> R) -> R -> R -> R -> R -> R -> R -> R
-trplIntg dt f x1 x2 y1 y2 z1 z2  =
-  dt^3*(f x2 y1 z1  + f x2 y2 z1  + f x2 y1 z2 + f x2 y2 z2  +
+trplIntg ::  Int -> (R -> R -> R -> R) -> R -> R -> R -> R -> R -> R -> R 
+trplIntg n f x1 x2 y1 y2 z1 z2  =
+  let dx=(x2-x1)/fromIntegral(2*n)
+      dy=(y2-y1)/fromIntegral(2*n)
+      dz=(z2-z1)/fromIntegral(2*n)
+  in dx*dy*dz*(f x2 y1 z1  + f x2 y2 z1  + f x2 y1 z2 + f x2 y2 z2  +
   f x1 y1 z1  + f x1 y2 z1  + f x1 y1 z2 + f x1 y2 z2)/8.0 +
-  ((sum [ (f x y1 z1)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt]]) +
-  (sum [ (f x y1 z2)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt]]) +
-  (sum [ (f x y2 z1)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt]]) +
-  (sum [ (f x y2 z2)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt]]) +
-  (sum [ (f x1 y z1)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x2 y z1)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x1 y z2)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x2 y z2)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x1 y1 z)*dt^3 | z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x2 y1 z)*dt^3 | z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x1 y2 z)*dt^3 | z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x2 y2 z)*dt^3 | z <- [z1+dt, z1+2*dt..z2-dt]]))/4.0 +
-  ((sum [ (f x y z1)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt], y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x y z2)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt], y <- [y1+dt, y1+2*dt..y2-dt]]) +
-  (sum [ (f x1 y z)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt], z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x2 y z)*dt^3 | y <- [y1+dt, y1+2*dt..y2-dt], z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x y1 z)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt], z <- [z1+dt, z1+2*dt..z2-dt]]) +
-  (sum [ (f x y2 z)*dt^3 | x <- [x1+dt, x1+2*dt..x2-dt], z <- [z1+dt, z1+2*dt..z2-dt]]))/2.0 +
-  sum [ (f x y z)*dt^3 | x <- [x1+dt,x1+2*dt..x2-dt], y <- [y1+dt, y1+2*dt..y2-dt],
-  z <- [z1+dt, z1+2*dt..z2-dt]]
+  ((sum [ (f x y1 z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
+  (sum [ (f x y1 z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
+  (sum [ (f x y2 z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
+  (sum [ (f x y2 z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
+  (sum [ (f x1 y z1)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x2 y z1)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x1 y z2)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x2 y z2)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x1 y1 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x2 y1 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x1 y2 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x2 y2 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]))/4.0 +
+  ((sum [ (f x y z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]])+
+  (sum [ (f x y z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x1 y z)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy], z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x2 y z)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy], z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x y1 z)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x y2 z)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], z <- [z1+dz, z1+2*dz..z2-dz]]))/2.0 +
+  sum [ (f x y z)*dx*dy*dz | x <- [x1+dx,x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy],
+  z <- [z1+dz, z1+2*dz..z2-dz]]
   
