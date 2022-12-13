12a25044e83d7b7ec9edd34043da7b8fcfc036f1
Jonathan <93748229+cleetus3142@users.noreply.github.com>
2022-12-13 02:04:26 -0700

    Update tripleIntgTrap.hs
    Added explanations

--- Integration/tripleIntgTrap.hs
+++ Integration/tripleIntgTrap.hs
@@ -32,14 +32,14 @@ trplIntg n f x1 x2 y1 y2 z1 z2  =
       dy=(y2-y1)/fromIntegral(2*n)
       dz=(z2-z1)/fromIntegral(2*n)
   in dx*dy*dz*(f x2 y1 z1  + f x2 y2 z1  + f x2 y1 z2 + f x2 y2 z2  +
-  f x1 y1 z1  + f x1 y2 z1  + f x1 y1 z2 + f x1 y2 z2)/8.0 +
+  f x1 y1 z1  + f x1 y2 z1  + f x1 y1 z2 + f x1 y2 z2)/8.0 +         -- 8 corners
   ((sum [ (f x y1 z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
   (sum [ (f x y1 z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
   (sum [ (f x y2 z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
   (sum [ (f x y2 z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx]]) +
   (sum [ (f x1 y z1)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
   (sum [ (f x2 y z1)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
-  (sum [ (f x1 y z2)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
+  (sum [ (f x1 y z2)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +  -- 12edges
   (sum [ (f x2 y z2)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy]]) +
   (sum [ (f x1 y1 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]) +
   (sum [ (f x2 y1 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]) +
@@ -47,10 +47,10 @@ trplIntg n f x1 x2 y1 y2 z1 z2  =
   (sum [ (f x2 y2 z)*dx*dy*dz | z <- [z1+dz, z1+2*dz..z2-dz]]))/4.0 +
   ((sum [ (f x y z1)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]])+
   (sum [ (f x y z2)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy]]) +
-  (sum [ (f x1 y z)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy], z <- [z1+dz, z1+2*dz..z2-dz]]) +
+  (sum [ (f x1 y z)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy], z <- [z1+dz, z1+2*dz..z2-dz]]) +  -- 6 faces 
   (sum [ (f x2 y z)*dx*dy*dz | y <- [y1+dy, y1+2*dy..y2-dy], z <- [z1+dz, z1+2*dz..z2-dz]]) +
   (sum [ (f x y1 z)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], z <- [z1+dz, z1+2*dz..z2-dz]]) +
-  (sum [ (f x y2 z)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], z <- [z1+dz, z1+2*dz..z2-dz]]))/2.0 +
+  (sum [ (f x y2 z)*dx*dy*dz | x <- [x1+dx, x1+2*dx..x2-dx], z <- [z1+dz, z1+2*dz..z2-dz]]))/2.0 +    -- The interior
   sum [ (f x y z)*dx*dy*dz | x <- [x1+dx,x1+2*dx..x2-dx], y <- [y1+dy, y1+2*dy..y2-dy],
   z <- [z1+dz, z1+2*dz..z2-dz]]
   
